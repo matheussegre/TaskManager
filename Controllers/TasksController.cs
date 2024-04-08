5402e4a9984aafe554535dbcaa1d0c92e924fb14
matheus <matheussegre.soares@gmail.com>
2024-04-08 10:36:15 -0300

    Add project files.

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.UseCases.Task.Delete;
+using TaskManager.Application.UseCases.Task.Get;
+using TaskManager.Application.UseCases.Task.Register;
+using TaskManager.Application.UseCases.Task.Update;
+using TaskManager.Communication.Request;
+using TaskManager.Communication.Response;
+
+namespace TaskManager.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TasksController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseGetTaskByIdJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseGetTaskByIdJson), StatusCodes.Status404NotFound)]
+    public IActionResult GetAllTasks()
+    {
+        var useCase = new GetTasksUseCase();
+
+        var response = useCase.Execute();
+
+        return Ok(response);
+    }
+
+    [HttpGet]
+    [Route("{taskId}")]
+    [ProducesResponseType(typeof(ResponseGetTaskByIdJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseGetTaskByIdJson), StatusCodes.Status404NotFound)]
+    public IActionResult GetTaskById([FromRoute] Guid taskId)
+    {
+        var useCase = new GetTaskByIdUseCase();
+
+        var response = useCase.Execute(taskId);
+
+        return Ok(response);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseCreateTaskJson),StatusCodes.Status201Created)]
+    public IActionResult CreateTask([FromBody] RequestTaskJson request)
+    {
+        var useCase = new RegisterTaskUseCase();
+
+        var response = useCase.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+
+    [HttpPut]
+    [Route("{taskId}")]
+    [ProducesResponseType(typeof(ResponseUpdateTaskJson), StatusCodes.Status204NoContent)]
+    public IActionResult UpdateTask([FromRoute] Guid taskId, [FromBody] RequestTaskJson request)
+    {
+        var useCase = new UpdateTaskUseCase();
+
+        useCase.Execute(taskId, request);
+
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Route("{taskId}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrosJson), StatusCodes.Status404NotFound)]
+    public IActionResult DeleteTask([FromRoute] Guid taskId)
+    {
+        var useCase = new DeleteTaskByIdUseCase();
+
+        useCase.Execute(taskId);
+
+        return NoContent();
+    }
+}
---
+++ Entities/Task.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.Communication.Enums;
+
+namespace TaskManager.API.Entities;
+
+public class Task
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Priority Priority { get; set; }
+    public DateOnly Deadline { get; set; }
+    public Status Status { get; set; } = Status.AWAITING;
+}
